4b9dc6e1941163551dab845578c159dd3ab04fa8
sevillecarlos <sevillecarlos@gmail.com>
2021-08-09 10:28:19 -0600

    user list routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,15 +5,28 @@ require 'jwt'
 module Api
   module V1
     class UsersController < ApplicationController
-      def index; end
+      def index
+        @users = User.all
+        render json: @users, status: 200
+      end
 
       def create
         @user = User.new(user_params)
         if @user.save
-          render json: { user: @user.username, password: @user.password, type: @user.user_type }
+          render json: { reason: 'You created user successfully' }, status: 200
         else
           render json: { error: @user.errors }, status: 400
         end
+      end
+
+      def update
+        @user = User.find(params[:id])
+        render json: true, status: 200 if @user.update(user_params)
+      end
+
+      def destroy
+        @user = User.find(params[:id])
+        render json: true, status: 200 if @user.delete
       end
 
       private
